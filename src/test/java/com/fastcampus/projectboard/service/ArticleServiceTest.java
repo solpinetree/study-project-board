39b8378cfc154d5c3e546032fae1138b2e2daebc
sol <sadlthf@icloud.com>
2022-08-11 12:39:41 +0900

    #28 - 게시글 페이지네이션 구현
    `이전 글`, `다음 글`만 구현하므로 게시판에 비해 단순하지만 
    첫 글, 마지막 글은 버튼 비활성화 및 가짜 링크 처리해야 해서 
    타임리프 문법이 복잡해짐
    
    마지막 글을 판단하기 위해 총 글 개수가 필요해졌고
    이를 위해 count 쿼리 사용, 서비스 로직 추가

--- src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleServiceTest.java
@@ -169,6 +169,21 @@ class ArticleServiceTest {
         then(articleRepository).should().deleteById(articleId);
     }
 
+    @DisplayName("게시글 수를 조회하면, 게시글 수를 반환한다")
+    @Test
+    void givenNothing_whenCountingArticles_thenReturnsArticlecount() {
+        //Given
+        long expected = 0L;
+        given(articleRepository.count()).willReturn(expected);
+
+        // When
+        long actual = sut.getArticleCount();
+
+        // Then
+        assertThat(actual).isEqualTo(expected);
+        then(articleRepository).should().count();
+    }
+
     private UserAccount createUserAccount() {
         return UserAccount.of(
                 "sol",
