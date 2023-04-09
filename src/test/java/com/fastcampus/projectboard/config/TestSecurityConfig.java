c2890bc70691d66b2ef2bb5713a83218925a4569
sol <sadlthf@icloud.com>
2023-04-09 21:54:56 +0900

    #66- 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
@@ -1,30 +1,37 @@
 package com.fastcampus.projectboard.config;
 
-import com.fastcampus.projectboard.domain.UserAccount;
-import com.fastcampus.projectboard.repository.UserAccountRepository;
-import org.mockito.ArgumentMatchers;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 // controller test 에서 사용
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod // 각 test code 가 실행되기 전에 인증 정보를 넣어줌.
     public void securitySetUp() {
-        given(userAccountRepository.findById(ArgumentMatchers.anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "unoTest",
                 "pw",
                 "uno-test@email.com",
                 "uno-test",
                 "test memo"
-        )));
+        );
     }
 }
