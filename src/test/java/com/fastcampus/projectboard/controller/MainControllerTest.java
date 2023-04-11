4375872a3f17d11c68ef984129938a64220a3c46
sol <sadlthf@icloud.com>
2023-04-11 09:58:05 +0900

    #67- application.yaml: 민감정보를 변수로 변경
    보안에 취약했떤 부분을 외부에서 집어넣을 수 있게끔 변경

--- src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/MainControllerTest.java
@@ -1,6 +1,7 @@
 package com.fastcampus.projectboard.controller;
 
 import com.fastcampus.projectboard.config.TestSecurityConfig;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@DisplayName("View 루트 컨트롤러")
 @Import(TestSecurityConfig.class)
 @WebMvcTest(MainController.class)
 class MainControllerTest {
